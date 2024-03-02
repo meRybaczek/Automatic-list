134994017b567009fa1269c49ce5f10f36f1eed3
PCzernicki <pablossik16@gmail.com>
2024-03-02 11:13:39 +0100

    Added methods to handle adding and deleting users, simple frontend added

---
+++ src/test/java/project/backend/UserServiceTest.java
@@ -0,0 +1,8 @@
+package project.backend;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@org.junit.jupiter.api.extension.ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
+class UserServiceTest {
+
+}
